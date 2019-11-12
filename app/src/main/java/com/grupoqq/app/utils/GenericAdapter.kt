c1e0f770a6ab7bb924789b9413161e9eeda9e5fc
_vegeuwu <josueardon.developer@gmail.com>
2019-11-12 16:49:36 -0600

    Implemented a generic RecyclerView adapter

---
+++ app/src/main/java/com/grupoqq/app/utils/GenericAdapter.kt
@@ -0,0 +1,34 @@
+package com.grupoqq.app.utils
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GenericAdapter<T>(
+    val layout: Int,
+    val list: MutableList<T>,
+    val action: (
+        viewHolder: GenericAdapter.ViewHolder,
+        view: View,
+        entry: T,
+        position: Int) -> Unit) : RecyclerView.Adapter<GenericAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenericAdapter.ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(layout, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        action.invoke(holder, holder.view, list[position], position)
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val view = view
+    }
+
+}
